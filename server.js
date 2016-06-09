3db1c8ca507a543dc053ad4133f32cfe26a0afed
jahdasha <jahdasha.flagg@gmail.com>
2016-06-09 00:30:32 -0400

    changed search page

--- server.js
+++ server.js
@@ -1,99 +1,117 @@
 // = Requirements ================================================================
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var request = require('request');
-var cheerio = require('cheerio');
+	var express = require('express');
+	var app = express();
+	var bodyParser = require('body-parser');
+	var logger = require('morgan');
+	var mongoose = require('mongoose');
+	var request = require('request');
+	var cheerio = require('cheerio');
 
 // = Middleware (pass everything through the logger first) ================================================
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
-app.use(express.static('public')); // (create a public folder and land there)
+	app.use(logger('dev'));
+	app.use(bodyParser.urlencoded({
+		extended: false
+	}));
+	app.use(express.static('public')); // (create a public folder and land there)
 
 // = Database configuration ================================================
-mongoose.connect('mongodb://localhost/mongoosescraper');
-var db = mongoose.connection;
+	mongoose.connect('mongodb://localhost/week18mongoosescraper');
+	var db = mongoose.connection;
 
-db.on('error', function (err) {
-console.log('Mongoose Error: ', err);
-});
-db.once('open', function () {
-console.log('Mongoose connection successful.');
-});
+	db.on('error', function (err) {
+		console.log('Mongoose Error: ', err);
+	});
+	db.once('open', function () {
+		console.log('Mongoose connection successful.');
+	});
 
-//Require Schemas
-var Note = require('./models/Note.js');
-var Article = require('./models/Article.js');
+// = Require Schemas ================================================================
+	var Note = require('./models/Note.js');
+	var Article = require('./models/Article.js');
+
+// = Routes ================================================================
+	app.get('/', function(req, res) {
+	  res.send(index.html); // sending the html file rather than rendering a handlebars file
+	});
+
+	// = api ================================================================
+
+	app.get('/scrape', function(req, res) {
+	  request('https://medium.com/browse/b99480981476', function(error, response, html) { // "medium's technology section"
+
+	    var $ = cheerio.load(html);
+	    $('article h2').each(function(i, element) {
+
+					var result = {};
+
+					result.title = $(this).children('a').text();
+					result.link = $(this).children('a').attr('href');
+
+					var entry = new Article (result);
+
+					entry.save(function(err, doc) {
+					  if (err) {
+					    console.log(err);
+					  } else {
+					    console.log(doc);
+					  }
+					});
 
 
-// Routes
-app.get('/', function(req, res) {
-  res.send(index.html);
-});
+	    });
+	  });
+	  res.send("Scrape Complete");
+	});
 
+	//Finish the route so it responds with all articles
+	app.get('/articles', function(req, res){
+		Article.find({}, function(err, doc){
+			if (err){
+				console.log(err);
+			} else {
+				res.json(doc);
+			}
+		});
+	});
 
-app.get('/scrape', function(req, res) {
-  request('http://www.echojs.com/', function(error, response, html) {
-    var $ = cheerio.load(html);
-    $('article h2').each(function(i, element) {
+		//Finish the route so it finds one article from the req.params.id,
+		//populates "note",
+		//and then responds with the article
+	app.get('/articles/:id', function(req, res){
+		Article.findOne({'_id': req.params.id})
+		.populate('note')
+		.exec(function(err, doc){
+			if (err){
+				console.log(err);
+			} else {
+				res.json(doc);
+			}
+		});
+	});
 
-				var result = {};
+		//save a new note
+		//then find an article from the req.params.id
+		//and updates "note" with the _id of the new note
+	app.post('/articles/:id', function(req, res){
+		var newNote = new Note(req.body);
 
-				result.title = $(this).children('a').text();
-				result.link = $(this).children('a').attr('href');
-
-				var entry = new Article (result);
-
-				entry.save(function(err, doc) {
-				  if (err) {
-				    console.log(err);
-				  } else {
-				    console.log(doc);
-				  }
+		newNote.save(function(err, doc){
+			if(err){
+				console.log(err);
+			} else {
+				Article.findOneAndUpdate({'_id': req.params.id}, {'note':doc._id})
+				.exec(function(err, doc){
+					if (err){
+						console.log(err);
+					} else {
+						res.send(doc);
+					}
 				});
 
+			}
+		});
+	});
 
-    });
-  });
-  res.send("Scrape Complete");
-});
-
-
-app.get('/articles', function(req, res){
-//Finish the route so it responds with all articles
-});
-
-
-app.get('/articles/:id', function(req, res){
-	//Finish the route so it finds one article from the req.params.id,
-
-	//populates "note",
-
-	//and then responds with the article
-
-});
-
-
-app.post('/articles/:id', function(req, res){
-	//save a new note
-
-	//then find an article from the req.params.id
-
-	//and updates "note" with the _id of the new note
-
-});
-
-
-
-
-
-
-
-
-app.listen(3008, function() {
-  console.log('App running on port 3008!');
-});
+	app.listen(3008, function() {
+	  console.log('App running on port 3008!');
+	});
